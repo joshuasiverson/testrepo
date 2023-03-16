eb0aa98e2ecce5e9f4fedc79677dee8f10ac9ac9
joshuasiverson <80544611+joshuasiverson@users.noreply.github.com>
2023-03-16 14:54:11 -0700

    Add files via upload

---
+++ main.py
@@ -0,0 +1,41 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+# print report - amount of water ml, milk ml, money $, coffee g
+# check resources sufficient
+# process coins - give change or refund
+
+
+MENU = {
+    "espresso": {
+        "ingredients": {
+            "water": 50,
+            "coffee": 18,
+        },
+        "cost": 1.5,
+    },
+    "latte": {
+        "ingredients": {
+            "water": 200,
+            "milk": 150,
+            "coffee": 24,
+        },
+        "cost": 2.5,
+    },
+    "cappuccino": {
+        "ingredients": {
+            "water": 250,
+            "milk": 100,
+            "coffee": 24,
+        },
+        "cost": 3.0,
+    }
+}
+
+resources = {
+    "water": 300,
+    "milk": 200,
+    "coffee": 100,
+}
+
